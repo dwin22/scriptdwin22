c3e444e6c2eb1b9711c38b4875ea8f4d87c63c53
dwin22 <49456607+dwin22@users.noreply.github.com>
2019-04-30 12:42:20 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -135,7 +135,7 @@ mv -f ${SCPinstal}/$1 ${ARQ}/$1
 chmod +x ${ARQ}/$1
 }
 fun_ip
-wget -O /usr/bin/trans https://raw.githubusercontent.com/dwin22/Generador/master/Install/trans &> /dev/null
+wget -O /usr/bin/trans https://raw.githubusercontent.com/dwin22/scriptdwin22/master/install/trans &> /dev/null
 msg -bar2
 msg -ama "[ NEW - ULTIMATE - SCRIPT ]"
 [[ $1 = "" ]] && funcao_idioma || {
